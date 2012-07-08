72879247992a488958c04091fc400d752b9cc845
Shaun Yelle <syelle@gmail.com>
2012-07-08 12:09:27 -0700

    remove old popover JS

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,3 @@
-$('[rel=popover]').popover({
-    animation: false, 
-    placement: get_popover_placement,
-    trigger: 'manual', 
-    delay: 0 
-}).click(function(){
-    $(this).popover('toggle')
-});
-
-function get_popover_placement(tip, element) {
-    var windowWidth = window.innerWidth;
-    var left_pos = $(element).offset().left;
-    if (windowWidth > 900 ) return 'top';
-    if (windowWidth - left_pos > 300) return 'right';
-    if (left_pos > 300) return 'left';
-    return 'top';
-}    
-
 /*
 $(document).scroll(function(){
     // If has not activated (has no attribute "data-top"
