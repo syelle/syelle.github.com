e01fa32bebede37ae031b10d3f08e538b954b586
Shaun Yelle <shaun@min-maxing.com>
2015-01-04 15:25:00 -0800

    update image URLs to point to full web subdomain

--- _plugins/_sitemap_generator.rb
+++ _plugins/_sitemap_generator.rb
@@ -39,7 +39,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://min-maxing.com"
+  MY_URL = "http://www.min-maxing.com"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
