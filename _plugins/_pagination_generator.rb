75af1f0d23221021bccb494a760fcb4ea9938287
Shaun Yelle <shaun@min-maxing.com>
2015-01-04 16:14:49 -0800

    fix pagination after upgrading Jekyll

--- _plugins/_pagination_generator.rb
+++ _plugins/_pagination_generator.rb
@@ -35,7 +35,7 @@ module Jekyll
       (1..pages).each do |num_page|
         pager = Pager.new(site.config, num_page, all_posts, pages)
         if num_page > 1
-          newpage = Page.new(site, File.join(site.source, "blog"), page.dir, page.name)
+          newpage = Page.new(site, File.join(site.source), page.dir, page.name)
           newpage.pager = pager
           newpage.dir = File.join(page.dir, "blog/page-#{num_page}")
           site.pages << newpage
