91942fb2d959774359ed5af4a856fd9742f3e98a
Shaun Yelle <shaun@min-maxing.com>
2023-04-07 11:02:20 -0700

    upgrade dependencies
    jquery: 1.7.2 -> 3.6.4
    bootstrap: 2.0.4 -> 5.3.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
- * @depends jquery-1.7.2.min.js
- * @depends bootstrap-2.0.4.min.js
+ * @depends jquery-3.6.4.min.js
+ * @depends bootstrap-5.3.0.min.js
  * @depends focus.js
  * @depends custom.js
  */
